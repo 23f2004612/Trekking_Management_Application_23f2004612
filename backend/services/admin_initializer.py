3ef6395807b1b59a73e38e5fe277c87d0f42ffe8
23f2004612 <23f2004612@ds.study.iitm.ac.in>
2026-07-13 17:43:27 +0530

    feat: add admin initialize service

---
+++ backend/services/admin_initializer.py
@@ -0,0 +1,22 @@
+import os
+from werkzeug.security import generate_password_hash
+from models.user import User
+from extensions import db
+
+def create_admin():
+
+    admin = User.query.filter_by(role="admin").first()
+
+    if admin:
+        return
+
+    admin = User(
+        full_name="Administrator",
+        email=os.environ.get("ADMIN_EMAIL"),
+        password=generate_password_hash(os.environ["ADMIN_PASSWORD"]),
+        role="admin",
+        status="active"
+    )
+
+    db.session.add(admin)
+    db.session.commit()    
